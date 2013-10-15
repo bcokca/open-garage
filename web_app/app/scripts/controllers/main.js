5b49eaf9b5b9477ffb1de0406e5f9b3a6c6d3045
Burhan <bcokca@gmail.com>
2013-10-15 01:13:09 -0700

    new angular project has been created via angular-manager plugins with yo

---
+++ web_app/app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('denemeBowerApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
