79f2d965414e4d5cffa7c2397fdeae1307e68dd2
Burhan <bcokca@gmail.com>
2013-10-18 21:42:23 -0700

    Map, NewGarage and List controllers added

--- web-app/app/scripts/controllers/main.js
+++ web-app/app/scripts/controllers/main.js
@@ -1,10 +1,25 @@
 'use strict';
 
-angular.module('denemeBowerApp')
+angular.module('openGarageApp')
   .controller('MainCtrl', function ($scope) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
-  });
+  })
+  .controller('ListCtrl', function($scope, $http){
+        //call server and get all garages
+       /** $http.get('http://localhost:3000/garage').
+            success(function(data, status, headers, config) {
+                $scope.garage_name = data;
+            }); **/
+    })
+    .controller('MapCtrl', function($scope){
+        //Load in a contact from the route (/contact/:index)
+        //call server and get all garages
+    })
+    .controller('NewGarageCtrl', function($scope){
+        //Load in a contact from the route (/contact/:index)
+        //call server and create new garages
+    });
