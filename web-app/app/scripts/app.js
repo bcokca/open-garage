7436e63c0b8f7639392785c667816221624120ac
Burhan <bcokca@gmail.com>
2013-10-18 21:41:03 -0700

    routers added for list, map and new sale

--- web-app/app/scripts/app.js
+++ web-app/app/scripts/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
-var app = angular.module('openGarageApp', []).config(function ($routeProvider) {
-
+var app = angular.module('openGarageApp', [])
+    .config(function ($routeProvider) {
     $routeProvider
         .when('/', {
             templateUrl: 'views/main.html',
@@ -10,6 +10,18 @@ var app = angular.module('openGarageApp', []).config(function ($routeProvider) {
         .when('/map', {
             templateUrl: 'views/map.html',
             controller: 'MapCtrl'
+        })
+        .when('/listGarages', {
+            templateUrl: 'views/listView.html',
+            controller: 'ListCtrl'
+        })
+        .when('/mapGarages', {
+            templateUrl: 'views/mapView.html',
+            controller: 'MapCtrl'
+        })
+        .when('/newGarageSale', {
+            templateUrl: 'views/newGarageView.html',
+            controller: 'NewGarageCtrl'
         })
         .otherwise({
             redirectTo: '/'
