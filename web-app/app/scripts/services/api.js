1f5e94564fbe9dc2f314607f3848db407983551a
Burhan <bcokca@gmail.com>
2013-10-19 19:27:41 -0700

    factory method created for garageService

--- web-app/app/scripts/services/api.js
+++ web-app/app/scripts/services/api.js
@@ -1,9 +1,24 @@
 var apiServices = angular.module('apiServices',['ngResource']);
 
 apiServices.factory('garageService', function($resource){
+    var getResource = function (url, params){
+
+        // if web_app running on server
+        if(ENV.type == 'local'){
+            params.port = ENV.port;
+            return  $resource(ENV.APIUrl + '::port' + url, params);
+        }
+
+        // if web_app running on server
+        else {
+            return  $resource(ENV.APIUrl + url, params);
+        }
+    }
+
     return {
         garage : function(){
-            return $resource('http://localhost::port/garage/:id', {id: '@id', port: 3000});
+            return getResource('/garage/:id', {id: '@id'});
         }
     };
 });
+
