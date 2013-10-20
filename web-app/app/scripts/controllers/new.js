1cd9a7d6eb701d554c7f952585eb3dfe90b9d537
Burhan <bcokca@gmail.com>
2013-10-19 23:18:42 -0700

    editing an existing garage.controller and view modified.

--- web-app/app/scripts/controllers/new.js
+++ web-app/app/scripts/controllers/new.js
@@ -1,23 +1,50 @@
 'use strict';
 
-app.controller('NewCtrl', function ($scope, $http, garageService,$location) {
+app.controller('NewCtrl', function ($scope, $http, garageService,$location,$routeParams) {
+
     $scope.garage = {};
 
-    $scope.addGarage = function () {
 
-        var resource = garageService.garage();
+    $scope.editMode=false;
 
-        new resource($scope.garage).$save(function(response){
+    if($routeParams.id){
+        $scope.editMode = true;
+        //first we need to find the garage object
+        garageService.garage().get({id: $routeParams.id}, function(response){
             if(response.status){
-                alert('New Garage Created');
-                $location.path('/detail/' + response.result[0]._id);
-            }
-            else {
-
-                console.log(response.error);
-
+                $scope.garage = response.result;
+            }else {
+                console.log(result.error);
             }
         });
+    }
+
+    //then update it
+    $scope.submitGarage = function () {
+        var resource = garageService.garage();
+        if($scope.editMode){
+
+            new resource($scope.garage).$save({id: $routeParams.id}, function(response){
+                if(response.status){
+                    alert('New Garage Created');
+                    $location.path('/detail/' + response.result._id);
+                }
+                else {
+                    console.log(response.error);
+                }
+            });
+        }else{
+            new resource($scope.garage).$save(function(response){
+                if(response.status){
+                    alert('New Garage Created');
+                    $location.path('/detail/' + response.result[0]._id);
+                }
+                else {
+                    console.log(response.error);
+                }
+            });
+        }
     };
+
 });
 
