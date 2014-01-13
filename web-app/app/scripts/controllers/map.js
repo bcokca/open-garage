ccdf6143815cb64114d8f1d79b6dc2a4a2558357
bcokca <bcokca@gmail.com>
2014-01-12 21:31:34 -0800

    getting current position

--- web-app/app/scripts/controllers/map.js
+++ web-app/app/scripts/controllers/map.js
@@ -1,38 +1,88 @@
 'use strict';
 
-app.controller('MapCtrl', function ($scope,  $timeout) {
+app.controller('MapCtrl', function ($scope,  $timeout, geolocation) {
+    //get my current location
+    var currLocation = {};
     // Enable the new Google Maps visuals until it gets enabled by default.
     // See http://googlegeodevelopers.blogspot.ca/2013/05/a-fresh-new-look-for-maps-api-for-all.html
     google.maps.visualRefresh = true;
+    geolocation.getCurrentPosition(function (position) {
+        alert('latitude:' + position.coords.latitude);
 
-    angular.extend($scope, {
-        position: {
+        $scope.position = {
             coords: {
-                latitude: 45,
-                longitude: -73
+                latitude: position.coords.latitude,
+                longitude: position.coords.longitude
             }
-        },
+        };
         /** the initial center of the map */
-        centerProperty: {
-            latitude: 45,
-            longitude: -73
-        },
+        $scope.centerProperty = {
+            latitude:  position.coords.latitude,
+            longitude: position.coords.longitude
+        }
         /** the initial zoom level of the map */
-        zoomProperty: 4,
+        $scope.zoomProperty = 4;
         /** list of markers to put in the map */
-        markersProperty: [ {
-            latitude: 45,
-            longitude: -74
-        }],
+        $scope.markersProperty= [ {
+            latitude:  position.coords.latitude,
+            longitude: position.coords.longitude
+        }];
         // These 2 properties will be set when clicking on the map
-        clickedLatitudeProperty: null,
-        clickedLongitudeProperty: null,
-        eventsProperty: {
+        $scope.clickedLatitudeProperty= null;
+        $scope.clickedLongitudeProperty= null;
+        $scope.eventsProperty = {
             click: function (mapModel, eventName, originalEventArgs) {
                 // 'this' is the directive's scope
                 console.log("user defined event on map directive with scope", this);
                 console.log("user defined event: " + eventName, mapModel, originalEventArgs);
             }
-        }
+        };
+
+
     });
+
+
+    $scope.position = {
+        coords: {
+            latitude: 45,
+            longitude: -73
+        }
+    };
+    /** the initial center of the map */
+    $scope.centerProperty = {
+        latitude: 45,
+        longitude: -73
+    }
+    /** the initial zoom level of the map */
+    $scope.zoomProperty = 4;
+    /** list of markers to put in the map */
+    $scope.markersProperty= [ {
+        latitude: 45,
+        longitude: -74
+    }];
+    // These 2 properties will be set when clicking on the map
+    $scope.clickedLatitudeProperty= null;
+    $scope.clickedLongitudeProperty= null;
+    $scope.eventsProperty = {
+        click: function (mapModel, eventName, originalEventArgs) {
+            // 'this' is the directive's scope
+            console.log("user defined event on map directive with scope", this);
+            console.log("user defined event: " + eventName, mapModel, originalEventArgs);
+        }
+    };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
